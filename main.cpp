f9bad1155f183254a00616c9329d774198b9155b
Noah <nkalma0001@gmail.com>
2026-07-31 14:32:44 -1000

    Added ball collision checking

--- main.cpp
+++ main.cpp
@@ -5,14 +5,16 @@
 #include <vector>
 #include <iostream>
 
-struct Particle
+struct Ball
 {
     Vector2 velocity;
     Vector2 position;
     int radius;
     Color color;
+    float mass;
+    static constexpr float density = 5;
 
-    Particle() {
+    Ball() {
         position = GetMousePosition();
         color = { (unsigned char)GetRandomValue(0, 255),
                     (unsigned char)GetRandomValue(0, 255),
@@ -20,10 +22,27 @@ struct Particle
                     255 };
         radius = GetRandomValue(10, 20);
         velocity = { 0, 0 };
+        mass = PI * radius * radius * density;
     };
 
 };
 
+// Calculates a partially inelastic collision between two balls
+Vector4 ballCollision(Ball ball1, Ball ball2, float e) { // Returns x,y,z,w (x,y is velocity 1 and z,w is velocity 2)
+    Vector4 finalVelocities = { 0, 0, 0, 0 };
+
+    finalVelocities.x = (ball1.mass * ball1.velocity.x + ball2.mass * ball2.velocity.x - ball2.mass * e * (ball1.velocity.x - ball2.velocity.x))
+                        / (ball1.mass + ball2.mass);
+    finalVelocities.y = (ball1.mass * ball1.velocity.y + ball2.mass * ball2.velocity.y - ball2.mass * e * (ball1.velocity.y - ball2.velocity.y))
+                        / (ball1.mass + ball2.mass);
+                        
+    finalVelocities.z = (ball1.mass * ball1.velocity.x + ball2.mass * ball2.velocity.x + ball1.mass * e * (ball1.velocity.x - ball2.velocity.x))
+                        / (ball1.mass + ball2.mass);
+    finalVelocities.w = (ball1.mass * ball1.velocity.y + ball2.mass * ball2.velocity.y + ball1.mass * e * (ball1.velocity.y - ball2.velocity.y))
+                        / (ball1.mass + ball2.mass);
+
+    return finalVelocities;
+}  
 
 int main() {
     
@@ -31,12 +50,13 @@ int main() {
     constexpr int screenHeight = 450;
     constexpr int gravSliderWidth = 200;
     constexpr int gravSliderHeight = 20;
+    constexpr int restitutionCoeff = 1;
     constexpr Vector2 gravSliderPos = { 25, 10 };
     Rectangle gravSliderRec = { gravSliderPos.x, gravSliderPos.y, gravSliderWidth, gravSliderHeight };
 
     float gravity = 0.1;
 
-    std::vector<Particle> particles {};
+    std::vector<Ball> balls {};
 
     InitWindow(screenWidth, screenHeight, "Particle Simulation");
 
@@ -55,29 +75,36 @@ int main() {
         // Places ball if mouse not on gravity slider
         if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT) && !CheckCollisionPointRec(GetMousePosition(), gravSliderRec))
         {
-            particles.emplace_back();
+            balls.emplace_back();
         }
 
+        for(Ball& ball : balls) { // loop through each particle
 
-        DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
-        for(Particle& particle : particles) { // loop through each particle
+            // Checks for ball collisions (TODO: OPTIMIZE)
+            for(Ball& checkBall : balls) {
+                if(&checkBall == &ball) continue;
+
+                if(CheckCollisionCircles(ball.position, ball.radius, checkBall.position, checkBall.radius)) {
+                    std::cout << "Collision" << std::endl;
+                }
+            }
 
             // Updates position from velocity
-            particle.position.x += particle.velocity.x;
-            particle.position.y += particle.velocity.y;
+            ball.position.x += ball.velocity.x;
+            ball.position.y += ball.velocity.y;
 
-            particle.velocity.y += gravity;
+            ball.velocity.y += gravity;
             
-            if(particle.position.y + particle.radius >= screenHeight) { // particle out of bounds below
-                particle.velocity.y = 0;
-                particle.position.y = screenHeight - particle.radius;
-            } else if(particle.position.y - particle.radius <= 0) { // particle out of bounds above
-                particle.velocity.y = 0;
-                particle.position.y = particle.radius;
+            if(ball.position.y + ball.radius >= screenHeight) { // ball out of bounds below
+                ball.velocity.y = 0;
+                ball.position.y = screenHeight - ball.radius;
+            } else if(ball.position.y - ball.radius <= 0) { // ball out of bounds above
+                ball.velocity.y = 0;
+                ball.position.y = ball.radius;
             }
     
-            // Draws each particle
-            DrawCircleV(particle.position, particle.radius, particle.color);
+            // Draws each ball
+            DrawCircleV(ball.position, ball.radius, ball.color);
         }
 
         EndDrawing();
